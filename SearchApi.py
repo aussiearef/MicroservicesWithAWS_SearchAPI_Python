f9bb671042d5efce5b2316a68cec0de597ce6c67
aussiearef <arefkr@gmail.com>
2023-06-22 03:42:16 +1000

    Debugged

--- SearchApi.py
+++ SearchApi.py
@@ -1,12 +1,11 @@
 from typing import List
 import os
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+import uvicorn
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search, Q
-from elasticsearch.exceptions import ConnectionError, TransportError
-from requests.exceptions import ConnectionError as RequestsConnectionError
-from time import sleep
+
 
 app = FastAPI()
 
@@ -15,56 +14,19 @@ es = Elasticsearch(
     http_auth=(os.environ.get("userName"), os.environ.get("password")),
 )
 
-circuit_breaker_policy = None
-
-try:
-    circuit_breaker_policy = CircuitBreakerPolicy(3, 30)
-except Exception as ex:
-    print(f"Failed to create CircuitBreakerPolicy: {ex}")
-
-
-class CircuitBreakerPolicy:
-    def __init__(self, max_failures: int, reset_timeout_seconds: int):
-        self.breaker = None
-        try:
-            self.breaker = Polly.CircuitBreaker.CircuitBreakerPolicy(
-                exceptions=[Exception()],
-                max_failures=max_failures,
-                duration_of_break=reset_timeout_seconds,
-            )
-        except Exception as ex:
-            print(f"Failed to create CircuitBreakerPolicy: {ex}")
-
-    async def execute_async(self, action):
-        if self.breaker is not None:
-            return await self.breaker.execute_async(action)
-        else:
-            return await action()
-
-
-@app.exception_handler(HTTPException)
-async def http_exception_handler(request, exc):
-    return JSONResponse(
-        status_code=exc.status_code, content={"message": exc.detail}
-    )
-
 
 @app.get("/search")
-async def search(city: str = None, rating: int = 1):
-    try:
-        result = await circuit_breaker_policy.execute_async(
-            lambda: search_hotels(city, rating)
-        )
-        return result
-    except Polly.CircuitBreaker.BrokenCircuitException as ex:
-        raise HTTPException(status_code=406, detail="Circuit is OPEN.")
-    except (ConnectionError, TransportError, RequestsConnectionError) as ex:
-        raise HTTPException(status_code=503, detail="Elasticsearch service is not available")
-    except Exception as ex:
-        raise HTTPException(status_code=500, detail="Internal server error")
+def search(city: str = None, rating: int = 1):
+    hotels =  search_hotels(city, rating)
+    response = JSONResponse(content= hotels, status_code=200, 
+                     headers={
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*"
+        }, media_type="application/json")
+    return response
 
-
-async def search_hotels(city: str, rating: int) -> List[dict]:
+def search_hotels(city: str, rating: int) :
     rating = rating or 1
 
     s = Search(using=es, index=os.environ.get("indexName"))
@@ -76,7 +38,9 @@ async def search_hotels(city: str, rating: int) -> List[dict]:
             Q("prefix", CityName={"value": city.lower()})
             & Q("range", Rating={"gte": rating})
         )
-
-    response = await s.execute()
+    response = s.execute()
     hotels = [hit.to_dict() for hit in response.hits.hits]
     return hotels
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- dockerfile
+++ dockerfile
@@ -11,8 +11,10 @@ COPY . /app
 RUN pip3 install --upgrade pip
 
 RUN pip3 install typing
+RUN pip3 install requests
 RUN pip3 install fastapi
 RUN pip3 install fastapi.responses
+RUN pip3 install uvicorn
 RUN pip3 install elasticsearch
 RUN pip3 install elasticsearch_dsl
 
@@ -22,6 +24,13 @@ RUN apt install -y curl
 
 # Make port 8080 available to the world outside this container
 EXPOSE 80
+
+# Set Environment Variables
+
+ENV host=https://hotels.es.us-central1.gcp.cloud.es.io:443
+ENV userName=elastic
+ENV password=Jx6IFj52PlDIujxrXLMaVTtP
+ENV indexName=hotels
 
 # Run the command to start the application
 CMD [ "python", "SearchApi.py" ]
